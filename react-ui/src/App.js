3c8609d4f85812f61e95f5f8f16636d0b669912b
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-08-04 02:12:54 -0600

    Header changes

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
     <div className="container">
       <h3 className="m-3 d-flex justify-content-center">
-       React JS Tutorial
+       COMIT Presentation
      </h3>
      <Navigation/>
      <Switch>
